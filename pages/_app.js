b8b9d4ac11b8892cabd3e3b81046d6134cb23442
William Monroy <william_monroy777@hotmail.com>
2022-05-14 00:25:46 -0500

    manifest.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import "../styles/globals.css";
+
+const myDarkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {},
+    space: {},
+    fonts: {},
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={myDarkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
